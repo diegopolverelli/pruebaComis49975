8dcaefa4c10cd430e422c9524702526e6948835f
Diego Polverelli <diegopolverelli@hotmail.com>
2023-10-27 21:48:59 -0300

    scritp 01 modificado

--- src/01-includesPotencia.js
+++ src/01-includesPotencia.js
@@ -1,4 +1,7 @@
 let numeros=[1,2,3,4,5]
+numeros=[1,2,3,4,5]
+numeros=[1,2,3,4,5]
+numeros=[1,2,3,4,5]
 
 let nombres=['Martina', 'Mariela', 'Sandra', 'Ana', 'Jimena', 'Marcelo', 'Julian', 'Ernesto']
 
